6084fa4500c5f1bf3d66cb0bdc8eefabbe74b00e
Sean Zheng <seanzhengw@gmail.com>
2018-01-13 14:56:29 +0800

    user: Add user_emails

---
+++ user/user_emails.go
@@ -0,0 +1,41 @@
+package user
+
+import (
+	"database/sql"
+)
+
+// CreateEmailsStatement of the user_emails table with (id, user_id, email)
+//
+// for the case with user may have multiple emails, and there is another table for users primary email
+const CreateEmailsStatement string = `
+CREATE TABLE IF NOT EXISTS user_emails (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	user_id INT UNSIGNED NOT NULL, 
+	email VARCHAR(100) NOT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT user_emails_id FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE CASCADE ON DELETE CASCADE
+)`
+
+// CreateEmailsStatementWithoutID of the user_emails table with (id, email)
+//
+// for the case with user may have multiple emails
+const CreateEmailsStatementWithoutID string = `
+CREATE TABLE IF NOT EXISTS user_emails (
+	id INT UNSIGNED NOT NULL, 
+	email VARCHAR(100) NOT NULL,
+	CONSTRAINT user_emails_id FOREIGN KEY (id) REFERENCES user (id) ON UPDATE CASCADE ON DELETE CASCADE
+)`
+
+// CreateEmails create user_emails table with (id, user_id, email) if not exist
+//
+// for the case with user may have multiple emails, and there is another table for users primary email
+func CreateEmails(sqldb *sql.DB) (sql.Result, error) {
+	return sqldb.Exec(CreateEmailsStatement)
+}
+
+// CreateEmailsWithoutID create user_emails table with (id, email) if not exist
+//
+// for the case with user may have multiple emails
+func CreateEmailsWithoutID(sqldb *sql.DB) (sql.Result, error) {
+	return sqldb.Exec(CreateEmailsStatementWithoutID)
+}
